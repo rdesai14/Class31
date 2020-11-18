1cf1a996c8c439cc2fb961105476cfb4f9713a22
Your Name <you@example.com>
2020-11-18 13:37:06 -0500

    done

--- Bird.js
+++ Bird.js
@@ -2,12 +2,20 @@ class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
+    this.smokeImage = loadImage("sprites/smoke.png");
+    this.arr1 = []
   }
 
   display() {
-    //this.body.position.x = mouseX;
-    //this.body.position.y = mouseY;
-
     super.display();
+    if(this.body.position.x>200 && this.body.velocity.x>10){
+    var arr2 = [this.body.position.x, this.body.position.y];
+    this.arr1.push(arr2);
+    }
+    for(var i = 0; i < this.arr1.length; i++){
+
+    image(this.smokeImage, this.arr1[i][0], this.arr1[i][1])
+
+    }
   }
 }
